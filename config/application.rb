14886cd336b2a892c6f9fa80e9513ae499e78e22
Sue Zheng Hao <szhlibrary@gmail.com>
2020-04-01 20:25:47 +0800

    Config: Add paths of files dealing with GitHub API to autoload.

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module GithubSearchAutocomplete
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.autoload_paths += %W(#{config.root}/lib/github_api)
+    config.autoload_paths += %W(#{config.root}/app/services)
   end
 end
