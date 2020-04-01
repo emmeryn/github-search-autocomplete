7320b0a0c86a800f34954de0297ad041a319698c
Sue Zheng Hao <szhlibrary@gmail.com>
2020-04-01 21:13:14 +0800

    Routes: Add main page containing autocomplete textbox.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'github#index'
+  get 'github/search_repositories/:query', to: 'github#search_repositories'
 end
